6d15462d9ec23de82227a3a451e0f5c60cdde322
unknown <dev.hannahk@gmail.com>
2021-01-10 02:45:24 +0900

    컴포넌트 분리하지 않았을 때 코드

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
-import TodoInput from './components/TodoInput';
-import TodoList from './components/TodoList';
+//import TodoInput from './components/TodoInput';
+//import TodoList from './components/TodoList';
+//import TodoItem from './components/TodoItem';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import uuid from 'react-uuid'
 
 class App extends Component {
-  state ={
-    items:[],
-    id:uuid(),
-    item: '',
-    editItem:false
+  constructor() {
+    super();
+    this.state ={
+      items:[],
+      id:uuid(),
+      item: '',
+      editItem:false
+    }
   }
+
 
   handleChange = (e) => {
     this.setState({
@@ -34,19 +39,49 @@ class App extends Component {
       editItem: false
     })
   }
+  
   render(){
     return (
       <div className="container">
         <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-4">
             <h3 className="text-capitalize text-center">todo Input</h3>
-            <TodoInput item={this.state.item} handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
-            <TodoList items={this.state.items} />
+            {/*<TodoInput item={this.state.item} handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>*/}
+            <div className="card card-body my-3">
+                <form onSubmit={ this.handleSubmit }>
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <div className="input-group-text bg-primary text-white">
+                                ❤
+                            </div>
+                        </div>
+                        <input value={ this.state.item } onChange={ this.handleChange } type="text" className="form-control text-capitalize" placeholder="add to do item"/>
+                    </div>
+                    <button type="submit" className="btn btn-block btn-primary mt-3">add item</button>
+                </form>
+            </div>
+            {/*<TodoList items={this.state.items} />*/}
+            <ul className="list-group my-5">
+                <h3 className="text-capitalize text-center">to do list</h3>
+                {this.state.items.map( item => {
+                    return(                       
+                        <li key={item.id} className="list-group-item text-capitalize d-flex justify-content-between my-2">
+                        <h6>{item.title}</h6>
+                        <div className="todo-icon">
+                        <span className="mx-2 text-success">✔</span>
+                        <span className="mx-2 text-success">❌</span> 
+                        </div>
+                        </li>)
+                })}
+                <button type="button" className="btn btn-danger btn-block text-capitalize mt-5">clear list</button>
+            </ul>
           </div>
         </div>
       </div>
     )
   }
 }
+
+{/*<TodoItem key={this.state.item.id} title={this.state.item.title} />*/}
 
 export default App;
